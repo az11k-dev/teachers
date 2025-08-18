7c884761ab317f9de55c3058e5b2485cf0af2769
az11k-dev <bazaclash55@gmail.com>
2025-08-18 19:20:09 +0500

    loaders-added

---
+++ src/app/[regionId]/[districtId]/loading.jsx
@@ -0,0 +1,13 @@
+// src/app/[regionId]/loading.js
+
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-50">
+            <div className="w-16 h-16 border-4 border-t-4 border-gray-200 rounded-full animate-spin border-t-indigo-500"></div>
+        </div>
+    );
+};
+
+export default Loader;
